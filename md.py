55bf1f9a14e0215f765151929712ead6db5a3ba3
beboche <davi.baux@inserm.fr>
2019-08-23 18:29:29 +0200

    added header and footer

--- md.py
+++ md.py
@@ -1,6 +1,7 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 app =  Flask(__name__)
 
 @app.route('/MD')
 def homepage():
-	return 'Welcome to Mobidetails!'
+	#return 'Welcome to Mobidetails!'
+	return render_template('homepage.html')
