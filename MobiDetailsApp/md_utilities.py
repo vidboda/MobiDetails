3f0bfc3fbeb47bfc08d07b65d7b4fb43d4fb4d4f
beboche <davi.baux@inserm.fr>
2019-12-09 11:06:36 +0100

    corrected text

--- MobiDetailsApp/md_utilities.py
+++ MobiDetailsApp/md_utilities.py
@@ -71,8 +71,8 @@ urls = {
 local_files = {
 	# id :[local path, version, name, short desc, urls Xref]
 	'clinvar_hg38': [app_path + '/static/resources/clinvar/hg38/clinvar_20190916.vcf.gz', 'v20190916', 'ClinVar', 'database of variants, clinically assessed', 'ncbi_clinvar'],
-	'gnomad_exome': [app_path + '/static/resources/gnomad/hg19_gnomad_exome_sorted.txt.gz', 'v2', 'gnomAD exome', 'large dataset of variants population frequencies', 'gnomad'],
-	'gnomad_genome': [app_path + '/static/resources/gnomad/hg19_gnomad_genome_sorted.txt.gz', 'v2', 'gnomAD genome', 'large dataset of variants population frequencies', 'gnomad'],
+	'gnomad_exome': [app_path + '/static/resources/gnomad/hg19_gnomad_exome_sorted.txt.gz', 'v2.0.1', 'gnomAD exome', 'large dataset of variants population frequencies', 'gnomad'],
+	'gnomad_genome': [app_path + '/static/resources/gnomad/hg19_gnomad_genome_sorted.txt.gz', 'v2.0.1', 'gnomAD genome', 'large dataset of variants population frequencies', 'gnomad'],
 	'dbscsnv': [app_path + '/static/resources/dbscSNV/hg19/dbscSNV.txt.gz', 'v1.1', 'dbscSNV', 'Dataset of splicing predictions', 'dbscsnv'],
 	#'spliceai': [app_path + '/static/resources/spliceai/hg19/exome_spliceai_scores.vcf.gz', 'v1.2.1', 'spliceAI', 'Dataset of splicing predictions', 'spliceai'],
 	'spliceai_snvs': [app_path + '/static/resources/spliceai/hg38/spliceai_scores.raw.snv.hg38.vcf.gz', 'v1.3', 'spliceAI SNVs', 'Dataset of splicing predictions', 'spliceai_snv'],
