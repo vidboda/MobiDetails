8fa15152699c9d31c2cb164758ebb7e1b2d8866f
beboche <davi.baux@inserm.fr>
2019-12-13 11:35:00 +0100

    updated reidrection to profile page

--- MobiDetailsApp/auth.py
+++ MobiDetailsApp/auth.py
@@ -120,7 +120,7 @@ def login():
 		if error is None:
 			session.clear()
 			session['user_id'] = user['id']
-			return redirect(url_for('index'))
+			return redirect(url_for('auth.profile'))
 
 		flash(error)
 
