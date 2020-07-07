96ae20a80274b6674e530b0a4f68bb601659e0a7
beboche <david.baux@inserm.fr>
2020-07-07 16:46:42 +0200

    added forgot password function

---
+++ MobiDetailsApp/static/javascript/auth_reset_pass.js
@@ -0,0 +1,18 @@
+function check_password_match() {
+    var password = $("#password").val();
+    var confirmPassword = $("#repassword").val();
+
+   if (password != confirmPassword) {
+      $("#divCheckPasswordMatch").html("<span style='color:red;'>Passwords do not match!</span>");
+      $('#sub').attr('disabled', 'disabled');
+   }
+   else {
+      $("#divCheckPasswordMatch").html("<span style='color:green;'>Passwords match.</span>");
+      $('#sub').removeAttr('disabled');
+   }
+}
+
+$(document).ready(function () {
+   $('#sub').attr('disabled', 'disabled');
+   $("#repassword").keyup(check_password_match);
+});
