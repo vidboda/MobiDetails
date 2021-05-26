073eb95bf31b61406820001c9f2957b6029fcf8c
beboche <david.baux@inserm.fr>
2021-05-26 09:23:55 +0200

    updated get_pos_splice_site_intron regexp for variants like "-385-172_-385-166delinsATT"

--- MobiDetailsApp/md_utilities.py
+++ MobiDetailsApp/md_utilities.py
@@ -292,7 +292,7 @@ def get_pos_splice_site_intron(name):
             match_obj.group(1)
         ]
     match_obj = re.search(
-        r'[\*-]?\d+([\+-])(\d+)_\d+[\+-](\d+)[^\d_]', name
+        r'[\*-]?\d+([\+-])(\d+)_[\*-]?\d+[\+-](\d+)[^\d_]', name
     )
     if match_obj:
         return [
