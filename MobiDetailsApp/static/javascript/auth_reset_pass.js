e42e7586f0f3eb5ad0f54b69f0b37e8e6d3ad6b5
beboche <david.baux@inserm.fr>
2023-04-26 17:15:25 +0200

    fixes potentially unsafe external links and password reset improvments

--- MobiDetailsApp/static/javascript/auth_reset_pass.js
+++ MobiDetailsApp/static/javascript/auth_reset_pass.js
@@ -2,17 +2,25 @@ function check_password_match() {
    var password = $("#password").val();
    var confirmPassword = $("#repassword").val();
 
-   if (password != confirmPassword) {
-      $("#divCheckPasswordMatch").html("<span style='color:red;'>Passwords do not match!</span>");
-      $('#sub').attr('disabled', 'disabled');
+   re = /(?!^[0-9]*$)(?!^[a-z]*$)(?!^[A-Z]*$)(?!^[a-zA-Z]*$)(?!^[a-z0-9]*$)(?!^[A-Z0-9]*$)^.{8,}$/;
+	if (re.test(password) === true && re.test(confirmPassword) === true) {
+      if (password != confirmPassword) {
+         $("#divCheckPasswordMatch").html("<span style='color:red;'>Passwords do not match!</span>");
+         $("#sub").attr("disabled", "disabled");
+      }
+      else {
+         $("#divCheckPasswordMatch").html("<span style='color:green;'>Passwords match.</span>");
+         $("#sub").removeAttr("disabled");
+      }
    }
    else {
-      $("#divCheckPasswordMatch").html("<span style='color:green;'>Passwords match.</span>");
-      $('#sub').removeAttr('disabled');
+      $("#divCheckPasswordMatch").html("<span style='color:red;'>Both passwords do not fill the required character mix and/or length</span>");
+      $("#sub").attr("disabled", "disabled");
    }
 }
 
 $(document).ready(function () {
-   $('#sub').attr('disabled', 'disabled');
+   $("#sub").attr("disabled", "disabled");
    $("#repassword").keyup(check_password_match);
+   $("#password").keyup(check_password_match);
 });
